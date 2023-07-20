762d85dd02ada168193a2e1e3efc0143fdb14400
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-20 15:51:10 -0600

    feat: Added basic layout and colors to nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,12 +3,14 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <>
+    <nav>
       <h1>humor me</h1>
-      <NavLink to='/'>generate</NavLink>
-      <NavLink to='/favorites'>favorites</NavLink>
-      <NavLink to='/create'>create</NavLink>
-    </>
+      <div className='nav-buttons'>
+      <NavLink to='/' className='nav-button'>😄 generate</NavLink>
+      <NavLink to='/favorites' className='nav-button'>💛 favorites</NavLink>
+      <NavLink to='/create' className='nav-button'>✏️ create</NavLink>
+      </div>
+    </nav>
   )
 }
 
