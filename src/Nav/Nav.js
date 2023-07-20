5a71f4ee46c20638444f3a1486219520b90523a9
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-20 14:55:04 -0600

    feat: Set up nav to check router is working

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,8 +1,14 @@
 import './Nav.css';
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <p>Nav here</p>
+    <>
+      <h1>humor me</h1>
+      <NavLink to='/'>generate</NavLink>
+      <NavLink to='/favorites'>favorites</NavLink>
+      <NavLink to='/create'>create</NavLink>
+    </>
   )
 }
 
