9c884cad5f70671ef9d9d769fa9dc0c1e1ec48e1
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-21 16:56:40 -0600

    feat: Complete building and styling for home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,9 +5,14 @@ const Home = () => {
   return (
     <div className='home-page'>
       <article className='selector' id='wholesome-block'>
-        <h3>wholesome quote generator</h3>
+        <h3 className='type-title'>wholesome quote generator</h3>
         <p className='selector-icon'>🥹</p>
-        <NavLink to='/'>click here!</NavLink>
+        <NavLink to='/wholesome-:posterid+quoteid' className='generate-link' id='generate-wholesome'>click here!</NavLink>
+      </article>
+      <article className='selector' id='chaotic-block'>
+        <h3 className='type-title'>chaotic quote generator</h3>
+        <p className='selector-icon'>😈</p>
+        <NavLink to='/chaotic-:posterid+quoteid' className='generate-link' id='generate-chaotic'>click here!</NavLink>
       </article>
     </div>
   )
