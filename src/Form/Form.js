de86755f6891e62fe80380a67d64b0aea595ca36
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-21 13:52:34 -0600

    feat: Set up form and state for quote submitter

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,9 +1,25 @@
 import './Form.css';
+import { useState } from 'react'
 
 const Form = () => {
+
+  const [formData, setFormData] = useState({
+    //need to add an id in here somewhere
+    type: '',
+    imgLink: '',
+    quote: '',
+  })
+
+  const handleSubmit = () => {
+    //code here for what to do when form is submitted
+  }
+
   return (
-    <div id='form-page'>
-      <p>Form here</p>
+    <div id='form-page' >
+      <h2>make your own quote!</h2>
+      <form onSubmit={handleSubmit}>
+        <label for='typeOfPoster'>1. select your quote type</label>
+      </form>
     </div>
   )
 }
