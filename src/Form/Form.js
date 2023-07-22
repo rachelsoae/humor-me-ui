7d3cf9dac99c625be7389af2537b374817748c73
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-21 18:47:15 -0600

    feat: Built and styled the form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -17,8 +17,20 @@ const Form = () => {
   return (
     <div id='form-page' >
       <h2>make your own quote!</h2>
+      <p>Your quote poster will be added to the quotes and can be generated randomly by future users.</p>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='typeOfPoster'>1. select your quote type</label>
+        <label htmlFor='type-of-poster'>1. select your quote type</label>
+        <div className='buttons'>
+          <button className='form-button' id='wholesome-button'>🥹 wholesome quote</button>
+          <button className='form-button' id='chaotic-button'>😈 chaotic quote</button>
+        </div>
+        <label htmlFor='img-url'>2. add you image url</label>
+        <input className='text-input' type='text' placeholder='image link goes here'></input>
+        <label htmlFor='img-url'>3. create your quote</label>
+        <input className='text-input' type='text' placeholder='quote goes here'></input>
+        <div className='buttons'>
+          <button className='form-button'>✏️ create</button>
+        </div>
       </form>
     </div>
   )
