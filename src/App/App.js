4dc2eba09bdeaf7b39b404d89c2b887a760a5862
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-24 14:11:14 -0600

    feat: Added psuedocode to components

--- src/App/App.js
+++ src/App/App.js
@@ -6,15 +6,23 @@ import Home from '../Home/Home.js'
 import Nav from '../Nav/Nav.js'
 
 const App = () => {
+
+  //when a random button is clicked:
+    //fetch all the quotes
+      //filter through them to sort by type
+      //select a random quote from the filtered array
+    //fetch all the images
+      //select random image
+    //go to the route - 'random-quote:quote-type' which displays the poster component with a dynamic background color and the poster with the image, quote, and fav button
+
   return (
-    <>
+    <> 
       <Nav />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='favorites' element={<Favorites />}/>
         <Route path='create' element={<Form />}/>
-        <Route path='/wholesome-:posterid+quoteid' element={<Favorites />}/>
-        <Route path='/chaotic-:posterid+quoteid' element={<Favorites />}/>
+        <Route path='/random-quote-:quote-type' element={<Favorites />}/>
       </Routes>
     </>
   )
--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -2,6 +2,9 @@ import './Favorites.css';
 import Card from '../Card/Card.js'
 
 const Favorites = () => {
+
+  //eventually we will bring the favorites in from the app, map through them, and then display them on the dom using the Cards component
+  
   return (
     <div id='fav-page'>
       <Card />
