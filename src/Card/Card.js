aae2ad1fd030abd2da036c27be9f8070b582ed13
Rachel Prather <rachelprather1@gmail.com>
2023-07-26 10:26:09 -0700

    refactor: Use grid and background image

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,12 +5,17 @@ const Card = ({image, quote, type}) => {
   // const backgroundImage = 'https://www.mattpaynephotography.com/images/960/San-Juan-Mountains-Sunrise-Panorama.webp' 
   
   return (
-    <article id='frame'>
-      <div id='img-and-quote' >
-        <img src={`${image}`} id='quote-img'/>
-        <p id='quote' >{`${quote}`}</p>
+    <article className='frame'>
+      <div 
+        className='img' 
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover'
+        }} >
+        <p className='quote' >{`${quote}`}</p>
       </div>
-      <div id='emojis'>
+      <div className='emojis'>
         <button className='card-button'>{type === 'wholesome' ? '🥹' : '😈'}</button>
         <button className='card-button'>🗑️</button>
       </div>
--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card.js'
 import { mockPosters } from '../mockData'
 
 const Favorites = () => {
-  const favorites = mockPosters.posters.map(poster => <Card image={poster.src} quote={poster.quote.text} type={poster.quote.type} />)
+  const favorites = mockPosters.posters.map(poster => <Card image={poster.src} quote={poster.quote.text} type={poster.quote.type} key={poster.id} />)
   
   return (
     <main id='fav-page'>
