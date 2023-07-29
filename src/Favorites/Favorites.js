45764f8fa77672283c7d280a66b9aba4d18faa29
Rachel Prather <rachelprather1@gmail.com>
2023-07-28 22:47:08 -0700

    feat: Add sad path message for when there are no favorites yet

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -5,8 +5,12 @@ const Favorites = ({favorites}) => {
   const cards = favorites.map(favorite => <Card key={favorite.id} image={favorite.src} quote={favorite.quote} type={favorite.type} font='1.3em'/>)
   
   return (
-    <main id='fav-page'>
-      {cards ? cards : (<h2 className='error-no-favorites'>You haven't saved any favorites yet!</h2>)}
+    <main className='fav-page'>
+      {cards.length > 0 ? (<div className='cards-grid'>{cards}</div>) : (
+        <div className='error-container no-favorites'>
+          <h2 className='error-message'>💛 You haven't saved any favorites yet! 💛</h2>
+        </div> 
+        )}
     </main>
   )
 }
