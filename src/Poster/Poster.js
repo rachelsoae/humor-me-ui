7baa1b504b01dc4322877ed16c5e0c0d414ec699
Rachel Prather <rachelprather1@gmail.com>
2023-07-30 12:08:25 -0700

    feat: Add default image

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -6,7 +6,7 @@ const Poster = ({poster, font, saveFavorite, randomizePoster, isFavorite}) => {
   return (
     <main className={`poster poster-${poster.type}`}>
       <div className='poster-container'>
-        <Card image={poster.image} quote={poster.quote} type={poster.type} font={font} />
+        <Card image={poster.image || 'https://images.unsplash.com/photo-1530721733923-e2df89901503?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'} quote={poster.quote} type={poster.type} font={font} />
       </div> 
       <div className='poster-buttons'>
         <button className='poster-button' onClick={() => randomizePoster('wholesome')}>🥹 random wholesome</button>
