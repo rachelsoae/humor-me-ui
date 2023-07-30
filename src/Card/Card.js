0b58811b642340a59e67588c1d114bf4c60b22ed
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-30 12:15:47 -0700

    refactor: Create conditional in Card component that defaults to a specific image if no image is loaded

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -7,7 +7,7 @@ const Card = ({image, quote, type, font}) => {
       <div 
         className='img' 
         style={{
-          backgroundImage: `url(${image})`,
+          backgroundImage: `url(${image || "https://images.unsplash.com/photo-1545389336-cf090694435e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"})`,
           backgroundPosition: 'center',
           backgroundSize: 'cover'
         }} 
