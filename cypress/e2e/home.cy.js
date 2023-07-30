aab6319f1a847bba5204a4ddda8f2a95bf6bfbfc
Daniel Cochico <dcochico.20@gmail.com>
2023-07-30 00:57:17 -0700

    feat: Add cypress test for nav button routing

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -14,7 +14,7 @@ describe('Home Page', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it.only('Should display title and buttons to generate, view favorites, and create posters', () => {
+  it('Should display title and buttons to generate, view favorites, and create posters', () => {
     cy.wait('@getQuotes').wait('@getImages').wait('@getPosters').then((interception) => {
       cy.get('.navbar').contains('h1', 'humor me')
         .get('.nav-buttons').children().should('have.length', 3)
@@ -24,5 +24,17 @@ describe('Home Page', () => {
     })
   })
 
+  it.only('Should click on nav buttons and take the user to their respective routes', () => {
+    cy.wait('@getQuotes').wait('@getImages').wait('@getPosters').then((interception) => {
+      cy.get('.nav-buttons').children().last().click()
+        .url().should('eq', 'http://localhost:3000/create')
+        .get('.nav-buttons').children().first().click()
+        .url().should('eq', 'http://localhost:3000/')
+        .get('.nav-buttons').children().eq(1).click()
+        .url().should('eq', 'http://localhost:3000/favorites')
+        .get('.navbar').find('h1').click()
+        .url().should('eq', 'http://localhost:3000/')
+    })
+  })
 
 })
