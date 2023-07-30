5306324f30e32bc687fe4a374199e12334073ee3
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-30 13:28:45 -0600

    feat: Added conditoonal logic if random poster page is refreshed to take user home instead of showing page with an undefined poster

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -1,19 +1,24 @@
 import './Poster.css';
 import Card from '../Card/Card'
+import Home from '../Home/Home'
 import { NavLink } from 'react-router-dom'
-// import { useState } from 'react'
 
 const Poster = ({poster, font, saveFavorite, randomizePoster, isFavorite}) => {
+
   return (
     <main className={`poster poster-${poster.type}`}>
-      <div className='poster-container'>
-        <Card image={poster.image} quote={poster.quote} type={poster.type} font={font} />
-      </div> 
-      <div className='poster-buttons'>
-        <NavLink className='poster-button' to="/poster/wholesome" onClick={() => randomizePoster('wholesome')}>🥹 random wholesome</NavLink>
-        <NavLink className='poster-button' to="/poster/chaotic" onClick={() => randomizePoster('chaotic')}>😈 random chaotic</NavLink>
-        {isFavorite ? <p className='poster-button saved'>Saved!</p> : <button className='poster-button' onClick={saveFavorite}>💛 save to favorites</button>}
-      </div>
+      {poster.type       
+      ? <>
+        <div className='poster-container'>
+          <Card image={poster.image} quote={poster.quote} type={poster.type} font={font} />
+        </div> 
+        <div className='poster-buttons'>
+          <NavLink className='poster-button' to="/poster/wholesome" onClick={() => randomizePoster('wholesome')}>🥹 random wholesome</NavLink>
+          <NavLink className='poster-button' to="/poster/chaotic" onClick={() => randomizePoster('chaotic')}>😈 random chaotic</NavLink>
+          {isFavorite ? <p className='poster-button saved'>Saved!</p> : <button className='poster-button' onClick={saveFavorite}>💛 save to favorites</button>}
+        </div> 
+      </>
+      : <NavLink to='/' className='poster-button'>Go Back to Generate Page</NavLink>}
     </main>
   )
 }
