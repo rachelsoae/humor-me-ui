4256e0d8ea975f293d1f77ab9f9f4c69b935d77d
Daniel Cochico <dcochico.20@gmail.com>
2023-07-30 02:47:16 -0700

    feat: Add cypress tests for favorites page

---
+++ cypress/e2e/favorites.cy.js
@@ -0,0 +1,47 @@
+describe('Favorites Page', () => {
+
+  const stubRequest = (url, code, fixture) => {
+    return cy.intercept('GET', `https://stretch-api.onrender.com/api/v1${url}`, {
+      statusCode: code,
+      fixture: fixture
+    });
+  }
+
+  beforeEach(() => {
+    stubRequest('/quotes', 200, 'quotes').as('getQuotes')
+    stubRequest('/images', 200, 'images').as('getImages')
+    stubRequest('/posters', 200, 'posters').as('getPosters')
+    cy.visit('http://localhost:3000/favorites')
+  })
+
+  it('Should display favorite posters and their respective images, quotes, and emojis', () => {
+    cy.wait('@getQuotes').wait('@getImages').wait('@getPosters').then((interception) => {
+      cy.get('.cards-grid').children().should('have.length', 3)
+        .get('.cards-grid').children().first().find('.img')
+        .get('.cards-grid').children().first().find('.quote')
+        .get('.cards-grid').children().first().find('.emojis')
+        .get('.cards-grid').children().last().find('.img')
+        .get('.cards-grid').children().last().find('.quote')
+        .get('.cards-grid').children().last().find('.emojis')
+    })
+  })
+
+  it('Should handle 404 errors and navigate the user back to the home page', () => {
+    cy.wait('@getQuotes').wait('@getImages').wait('@getPosters').then((interception) => {
+      cy.visit('http://localhost:3000/error')
+        .get('.error-message').should('have.text', "🤕  Uh-oh... There's been an error  🤕")
+        .get('#error-home-button').should('have.text', '😄 go home').click()
+        .url().should('eq', 'http://localhost:3000/')
+    })
+  })
+
+  it('Should handle 500 level errors', () => {
+    stubRequest('/quotes', 500, 'quotes').as('dropQuotes')
+    stubRequest('/images', 500, 'images').as('dropImages')
+    stubRequest('/posters', 500, 'posters').as('dropPosters')
+    cy.wait('@dropQuotes').wait('@dropImages').wait('@dropPosters').then((interception) => {
+      cy.get('.error-message').should('have.text', "🤕  Uh-oh... There's been an error  🤕")
+        .get('#error-home-button').should('have.text', '😄 go home')
+    })
+  })
+})
